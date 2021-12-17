a0d3340e86885a9aeff23b2073a1b5fb3401aa5a
Glenn <glennwiz@gmail.com>
2021-12-17 16:30:33 +0100

    added keyboard controlls

--- Game1.cs
+++ Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -6,11 +7,12 @@ namespace TestingMonoGame
 {
     public class Game1 : Game
     {
-        private Texture2D ballTexture;
         
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-
+        Texture2D ballTexture;
+        Vector2 ballPosition;
+        float ballSpeed;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -20,7 +22,19 @@ namespace TestingMonoGame
 
         protected override void Initialize()
         {
+            int?  x = 1;
+            x = null;
+            x = 2;
+
+            DateTime? zz = null;
+            zz = DateTime.Now;
+            zz = null;
+            
+            
             // TODO: Add your initialization logic here
+            ballPosition = new Vector2(graphics.PreferredBackBufferWidth / 2,
+                graphics.PreferredBackBufferHeight / 2);
+            ballSpeed = 100f;
 
             base.Initialize();
         }
@@ -39,7 +53,30 @@ namespace TestingMonoGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            
             // TODO: Add your update logic here
+            var kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Up))
+                ballPosition.Y -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(kstate.IsKeyDown(Keys.Down))
+                ballPosition.Y += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.Left))
+                ballPosition.X -= ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
+            if(kstate.IsKeyDown(Keys.Right))
+                ballPosition.X += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(ballPosition.X > graphics.PreferredBackBufferWidth - ballTexture.Width / 2)
+                ballPosition.X = graphics.PreferredBackBufferWidth - ballTexture.Width / 2;
+            else if(ballPosition.X < ballTexture.Width / 2)
+                ballPosition.X = ballTexture.Width / 2;
+
+            if(ballPosition.Y > graphics.PreferredBackBufferHeight - ballTexture.Height / 2)
+                ballPosition.Y = graphics.PreferredBackBufferHeight - ballTexture.Height / 2;
+            else if(ballPosition.Y < ballTexture.Height / 2)
+                ballPosition.Y = ballTexture.Height / 2;
 
             base.Update(gameTime);
         }
@@ -49,9 +86,19 @@ namespace TestingMonoGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
              spriteBatch.Begin();
-             spriteBatch.Draw(ballTexture, new Vector2(0, 0), Color.White);
+             
+             spriteBatch.Draw(ballTexture,
+                 ballPosition,
+                 null,
+                 Color.White,
+                 0f,
+                 new Vector2(ballTexture.Width / 2, ballTexture.Height / 2),
+                 Vector2.One,
+                 SpriteEffects.None,
+                 0f);
+             
              spriteBatch.End();
-
+            
             base.Draw(gameTime);
         }
     }
